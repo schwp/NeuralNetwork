a7ca3e9eb78857f43925813e0e57a0035d33c168
Pierre SCHWEITZER <pierre.schweitzer@epita.fr>
2025-01-20 15:17:28 +0100

    fix(test): adding while loop and images display

--- NeuralNetworkMnist/test.py
+++ NeuralNetworkMnist/test.py
@@ -10,11 +10,17 @@ def plot_images(arr, image_name):
         pyplot.imshow(arr[i], cmap='gray')
         pyplot.savefig(image_name)
 
+def plot_image(arr, i):
+    first_image = np.array(arr[i], dtype='float')
+    pixels = np.where(first_image == 0, 1, 0).reshape((28, 28))
+    for i in range(28):
+        print(pixels[i])
+
 if __name__ == "__main__":
     print("=== MNIST NEURAL NETWORK TEST ===")
     (train_X, train_y), (test_X, test_y) = mnist.load_data()
-    plot_images(train_X, "train_images.png")
-    plot_images(test_X, "test_images.png")
+    #plot_images(train_X, "train_images.png")
+    #plot_images(test_X, "test_images.png")
 
     X = []
     for im in train_X:
@@ -33,6 +39,15 @@ if __name__ == "__main__":
     nn = NeuralNetworkMnist(X[:10000], Y[:10000], 0.0001)
     nn.train()
 
+    size = len(test_y)
+    while True:
+        valueToTest = int(input(f'Image index to test (max {size}): '))
+        if valueToTest < 0 : break
+        guess_value = nn.predict(test_X[valueToTest].flatten())[0]
+        print(f'Guessed value : {guess_value}')
+        plot_image(test_X, valueToTest)
+
+    """
     correct = 0
     total = len(test_y)
     for i in  range(total):
@@ -40,3 +55,4 @@ if __name__ == "__main__":
             correct += 1
 
     print(f'Coverage: {round(correct * 100 / total)}% ({correct}/{total})')
+    """
